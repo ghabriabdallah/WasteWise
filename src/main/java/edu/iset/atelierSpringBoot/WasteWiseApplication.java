b03daea878ac0ddb7b05505926cb9840954b910b
Dhia Borji <dhia.borji2001@gmail.com>
2023-02-12 15:33:09 +0100

    first commit backend repository

---
+++ src/main/java/edu/iset/atelierSpringBoot/WasteWiseApplication.java
@@ -0,0 +1,13 @@
+package edu.iset.atelierSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iset/atelierSpringBoot/WasteWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iset.atelierSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
