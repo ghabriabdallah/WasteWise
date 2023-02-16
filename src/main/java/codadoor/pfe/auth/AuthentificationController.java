b1bc817b5ca8c757cb756ef21fde6a2e234065e9
abdallah ghabri <abdallah.ghabri@outlook.fr>
2023-02-16 20:11:39 +0100

    added cors policty

--- src/main/java/codadoor/pfe/auth/AuthentificationController.java
+++ src/main/java/codadoor/pfe/auth/AuthentificationController.java
@@ -1,6 +1,7 @@
 package codadoor.pfe.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,12 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/WasteWise/auth/")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = {"Content-Type"})
 public class AuthentificationController {
 
 	private final AuthenticationService service;
 	
+	@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = {"Content-Type"})
 	@PostMapping("/register")
 	public ResponseEntity<AuthetificationResponse> register(
 			@RequestBody RegisterRequest request){
@@ -23,7 +26,7 @@ public class AuthentificationController {
 	}
 
 
-
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = {"Content-Type"})
 @PostMapping("/authenticate")
 public ResponseEntity<AuthetificationResponse> authenticate(
 		@RequestBody AuthetificationRequest request){
