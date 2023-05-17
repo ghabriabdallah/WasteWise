9b3a9a99eee576c32e6cbdf6560f755c42c680a5
Abdallah Ghabri <abdallah.ghabri@outlook.fr>
2023-05-17 17:04:33 +0100

    added relations

--- src/main/java/codadoor/pfe/entity/Feedback.java
+++ src/main/java/codadoor/pfe/entity/Feedback.java
@@ -7,8 +7,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -31,7 +33,8 @@ public class Feedback {
     @Column(length = 1000)
     private String message;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User user;
     
    
